5d6508292c381b3a57c87b74a88bffec32227033
hakan <erik.hakansson96@gmail.com>
2022-08-09 15:38:57 +0200

    add responses

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,12 @@
 from turnin import config
 
 
-valid_provider = {1: "GitHub"}
-valid_email = "example@example.com"
 valid_config = {
-    "provider": valid_provider[1],
-    "user_email": valid_email,
-    "instructor_email_addresses": [valid_email],
+    "provider": "GitHub",
+    "user_email": "example@example.com",
+    "instructor_email_addresses": ["example@example.com"],
 }
+provider, email, instructor = valid_config.values()
 
 
 def test_exists(configuration: config.Configuration):
@@ -16,20 +15,20 @@ def test_exists(configuration: config.Configuration):
 
 def test_should_retry_with_invalid_provider(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda x: "1")
-    provider = config.Configuration.prompt_provider("some provider idk")
-    assert provider == valid_provider[1]
+    selected_provider = config.Configuration.prompt_provider("some provider idk")
+    assert provider == selected_provider
 
 
 def test_should_retry_with_invalid_email(monkeypatch):
-    monkeypatch.setattr("builtins.input", lambda x: valid_email)
-    email = config.Configuration.prompt_email("miss", "matched")
-    assert email == valid_email
+    monkeypatch.setattr("builtins.input", lambda x: email)
+    selected_email = config.Configuration.prompt_email("miss", "matched")
+    assert email == selected_email
 
 
 def test_should_allow_valid_instructor(monkeypatch):
-    monkeypatch.setattr("builtins.input", lambda x: valid_email)
+    monkeypatch.setattr("builtins.input", lambda x: email)
     instructors = config.Configuration.prompt_instructors()
-    assert instructors[0] == valid_email
+    assert instructors[0] == email
 
 
 def test_should_not_overwrite_unless_explicit(monkeypatch):
--- tests/test_github.py
+++ tests/test_github.py
@@ -1,4 +1,30 @@
-from turnin.github import GitHub
+import pytest
+from turnin.github import (
+    GitHub, 
+    OauthChallengeResponse,
+    DeviceAccessTokenResponse
+)
+
+
+@pytest.fixture
+def oauth_response() -> OauthChallengeResponse:
+    return OauthChallengeResponse(**{
+        "device_code": "",
+        "user_code": "",
+        "verification_uri": "",
+        "expires_in": 5,
+        "interval": 5,
+    })
+
+@pytest.fixture
+def access_token_response() -> DeviceAccessTokenResponse:
+    return OauthChallengeResponse(**{
+        "device_code": "",
+        "user_code": "",
+        "verification_uri": "",
+        "expires_in": 5,
+        "interval": 5,
+    })
 
 
 def test_exists(github: GitHub):
